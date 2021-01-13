c312ea502198d5ddd0e19f52a8f26ecbbb44e3ca
Pastaroom Mapo <pastaroom.mapo@gmail.com>
2021-01-13 22:33:29 +0900

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+}
+</script>
+
+<style scoped>
+
+</style>
