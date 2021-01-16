757890d770a146f039444a6d7950b49f05341fbb
Pastaroom Mapo <pastaroom.mapo@gmail.com>
2021-01-16 12:26:30 +0900

    check

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,17 +6,14 @@ Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
+    path: '/',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   }
 ]
 
